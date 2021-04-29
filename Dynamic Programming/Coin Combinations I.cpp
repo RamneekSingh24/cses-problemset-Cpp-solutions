37f25a9d27b77f0d16e44bc5e60ec0881abd6872
RamneekSingh24 <74413910+RamneekSingh24@users.noreply.github.com>
2021-04-29 23:01:01 +0530

    Create Coin Combinations I.cpp

---
+++ Dynamic Programming/Coin Combinations I.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <memory.h>
+#include <algorithm>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+typedef pair<int,int> pii;
+
+const int MOD = 1e9 + 7;
+
+
+int main() {
+
+#ifdef MYPC
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+#endif
+
+    fast_io;
+
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    vector<int> dp(x+1,0);
+    dp[0] = 1;
+    for(int i = 1 ; i <= x; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i - v[j] < 0) {
+                continue;
+            }
+            dp[i] += dp[i - v[j]];
+            if(dp[i] >= MOD) {
+                dp[i] -= MOD;
+            }
+        }
+    }
+
+    cout << dp[x] << endl;
+
+    return 0;
+}
+
+
+
+
+
