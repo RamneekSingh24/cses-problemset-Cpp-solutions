5a7a901a92d16eec4d935ef441120b34b9e1128c
RamneekSingh24 <74413910+RamneekSingh24@users.noreply.github.com>
2021-05-02 01:12:26 +0530

    Update Counting Towers.cpp

--- Dynamic Programming/Counting Towers.cpp
+++ Dynamic Programming/Counting Towers.cpp
@@ -42,10 +42,9 @@ int main() {
     dp1[1] = 1;
     dp2[1] = 1;
 
-    int sumdp = 2;
+    int sumdp = 3;
 
     for(int i = 2; i < sz; i++) {
-        dp2[i] = 1;
         add(dp1[i], dp1[i-1]);
         add(dp1[i], dp1[i-1]);
         add(dp1[i], dp1[i-1]);
